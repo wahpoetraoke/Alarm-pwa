ab80f75d5b64a583ddae7bba54c44a64214b94ca
wahpoetraoke <triwahyoepoetra@gmail.com>
2026-09-08 14:52:07 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,6 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('notificationclick', event => {
-  event.notification.close();
-  event.waitUntil(clients.openWindow('./'));
-});
-self.addEventListener('message', event => {
-  if (event.data.type === 'ALARM_TRIGGER') {
-    self.registration.showNotification('⏰ ' + event.data.label, {
-      body: `Alarm jam ${event.data.time}`,
-      icon: 'icon-512.png',
-      badge: 'icon-192.png',
-      vibrate: [500,200,500],
-      tag: 'alarm-notif'
-    });
-    setTimeout(() => {
-      self.registration.getNotifications({tag:'alarm-notif'}).then(n=>n.forEach(x=>x.close()));
-    }, 5000);
-  }
+self.addEventListener('install', e=>self.skipWaiting());
+self.addEventListener('activate', e=>self.clients.claim());
+self.addEventListener('notificationclick', e=>{
+  e.notification.close();
+  e.waitUntil(clients.openWindow('./'));
 });
