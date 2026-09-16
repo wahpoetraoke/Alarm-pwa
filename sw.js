22e6cde63ca6b4424ee38db3f1cd9125e015cd23
wahpoetraoke <triwahyoepoetra@gmail.com>
2026-09-16 21:31:13 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e=>self.skipWaiting());
-self.addEventListener('activate', e=>self.clients.claim());
-self.addEventListener('notificationclick', e=>{
-  e.notification.close();
-  e.waitUntil(clients.openWindow('./'));
-});
